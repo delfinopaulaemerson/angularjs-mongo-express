ba0eab7e1edbb544623080e51c3b92872f06b4bb
delfinopaulaemerson <delfs@hotmail.com>
2019-09-28 12:36:05 -0300

    Angular JS - Mongo - Express - NodeRestful

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,13 +3,14 @@ const bodyParse = require('body-parser');
 const express = require('express');
 const server = express();
 const allowCors = require('./cors');
-
+const queryParser = require('express-query-int');
 //bodyparse vai ser responsavel pelos dados da submissao de um formulario
 server.use(bodyParse.urlencoded({ extended: true }));
 
 //bodyparse vai fazer uma concersao de json para objeto
 server.use(bodyParse.json());
 server.use(allowCors);
+server.use(queryParser())
 
 server.listen(porta, function() {
   console.log(`Backend running on port ${porta}`);
